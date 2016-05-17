f427881b1d8fd2bdf1dcdc10f5a4bed58eab43fa
Banzay <urkobanzay@gmail.com>
2016-05-17 16:48:07 +0300

    1

--- magento-script.sh
+++ magento-script.sh
@@ -15,7 +15,7 @@ add-apt-repository -y ppa:ondrej/php
 wait
 apt-get -y update
 apt-get -y install nginx
-apt-get -y install php7.0 php7.0-fpm php7.0-mysql php7.0-zip php7.0-gd php-xml  php7.0-mcrypt
+apt-get -y install php7.0 php7.0-soap php7.0-fpm php7.0-mysql php7.0-zip php7.0-gd php7.0-xsl php-xml php7.0-mcrypt php7.0-curl php7.0-intl php7.0-mbstring php7.0-json 
 wait
 #take mysql root pass
 
