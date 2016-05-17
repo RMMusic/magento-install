604eb885c2b0679c9a5ebeb078fcf694aad3a13c
Banzay <urkobanzay@gmail.com>
2016-05-17 22:39:13 +0300

    5

--- config-nginx.sh
+++ config-nginx.sh
@@ -6,21 +6,16 @@ sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$domainnam
 
 echo "
 server {
-        listen 80 $domainname;
-        listen [::]:80 $domainname ipv6only=on;
+        listen 80 default_server;
+        listen [::]:80 default_server ipv6only=on;
 
         root $homedir/html;
         index index.html index.htm;
 
-        # Make site accessible from http://localhost/
-        server_name localhost;
+        server_name $domainname www.$domainname;
 
         location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files $uri $uri/ =404;
-                # Uncomment to enable naxsi on this location
-                # include /etc/nginx/naxsi.rules
+           try_files $uri $uri/ =404;
         }
 
 " > /etc/nginx/sites-available/$domainname
