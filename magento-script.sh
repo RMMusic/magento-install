6dbffe59e77c546608d50fded25b0bda446b867f
Banzay <urkobanzay@gmail.com>
2016-05-26 19:19:27 +0300

    6

--- magento-script.sh
+++ magento-script.sh
@@ -1,6 +1,8 @@
 ﻿#!/bin/bash
 export homedir=$1
 export domainname=$2
+#export dbname=
+#export dbpass=
 export MAGE_ROOT=$homedir$domainname;
 export MAGE_MODE=default;
 echo "our home dir:"$1
@@ -18,9 +20,6 @@ apt-get -y update
 apt-get -y install nginx
 apt-get -y install php7.0 php7.0-soap php7.0-fpm php7.0-mysql php7.0-zip php7.0-gd php7.0-xsl php-xml php7.0-mcrypt php7.0-curl php7.0-intl php7.0-mbstring php7.0-json 
 wait
-#take mysql root pass
-#echo "Please enter mysql root pass: "
-#read MYSQL_ROOT_PASS
 export MYSQL_ROOT_PASS=admin123
 apt-get -y install debconf-utils
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASS"
@@ -33,9 +32,7 @@ wait
 /bin/bash magento-install-composer.sh
 wait
 /bin/bash install-magento.sh
-
-
-
+wait
 sudo php $homedir$domainname/bin/magento sampledata:deloy
 wait
 sudo php $homedir$domainname/bin/magento setup:upgrade
