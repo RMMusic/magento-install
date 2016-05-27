025c28f3c6acea067e2ecffeb3e8a757827c698a
Banzay <urkobanzay@gmail.com>
2016-05-27 14:26:39 +0300

    7

--- config-nginx.sh
+++ config-nginx.sh
@@ -1,6 +1,9 @@
 ﻿#!/bin/bash
 sudo echo "10.1.1.210	$domainname" >> /etc/hosts
 sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$domainname
+echo "$(echo '
+set MAGE_ROOT $homedir$domainname;
+set MAGE_MODE default;' | cat $homedir$domainname/nginx.conf.sample)" >  $homedir$domainname/nginx.conf.sample
 echo "
 upstream fastcgi_backend {
      server 127.0.0.1:9000;
@@ -9,8 +12,6 @@ upstream fastcgi_backend {
 server {
     listen 80;
     server_name $domainname;
-	set '$MAGE_ROOT' $homedir$domainname;
-    set '$MAGE_MODE' default;
     include /var/www/m.com/nginx.conf.sample;
        }
 " > /etc/nginx/sites-available/$domainname
